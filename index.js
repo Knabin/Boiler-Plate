2a8e3dcf7bd63887cfe955c6106eee40c87bf5b4
Knabin <knabinn97@gmail.com>
2022-02-24 13:58:31 +0900

    DB 연결 시 dotnet config를 참조
    * uri가 노출되어서는 안 된다. 주의.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
 const port = 5000
 
+dotenv.config()
+const uri = process.env.MONGO_URI
+
+console.log("URI: ", process.env.MONGO_URI)
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Knabin:abcd1234@boilerplate.wujdk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(uri)
 .then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
