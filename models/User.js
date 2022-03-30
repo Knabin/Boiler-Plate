826888030f9e4265cbdf5ff3bc273d54bad260b8
Knabin <knabinn97@gmail.com>
2022-03-30 22:49:39 +0900

    로그아웃 기능 구현 (完)

--- models/User.js
+++ models/User.js
@@ -90,7 +90,7 @@ userSchema.statics.findByToken = function(token, cb) {
         // 클라이언트에서 가져온 token과 DB에 보관된 token이 일치하는지 확인한다.
         user.findOne({ "_id" : decoded, "token": token }, function(err, user) {
             if (err) return cb(err)
-            cd(null, user)
+            cb(null, user)
         })
     })
 }
